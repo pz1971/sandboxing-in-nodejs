f61ed9bdafb68b2b496e87e94fcbd12395a01e1c
Parvez Mia <parvez.pz1971@gmail.com>
2022-03-24 19:03:03 +0600

    implemented vm2

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const {judgeCpp} = require("./judge.js")
-
-judgeCpp("./OJ/solution.cpp")
+// const {judgeCpp} = require("./judge.js")
+// judgeCpp("./OJ/solution.cpp")
 // judgeCpp("./OJ/WA_solution.cpp")
 // judgeCpp("./OJ/TLE_solution.cpp")
 // judgeCpp("./OJ/MLE_solution.cpp")
+
+const {VM, NodeVM, VMScript} = require('vm2');
+
+const vm = new NodeVM({
+  console: 'inherit',
+    sandbox: {},
+    require: {
+        external: true,
+        builtin: ['fs', 'path'],
+        root: './'
+    }
+});
+const script = new VMScript('const {judgeCpp} = require("./judge.js"); module.exports = judgeCpp("./OJ/solution.cpp");');
+// console.log(vm.run(script));
+vm.run(script);
